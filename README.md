5504fe376f108d937b1669675a8403f12c510473
sleepycrow <sleepycrow@posteo.net>
2026-07-15 21:32:47 +0200

    Misc. style improvements

---
+++ README.md
@@ -0,0 +1,7 @@
+# Catstat
+
+A companion site for [Lynxsie's](https://www.twitch.tv/lynxsiethewarmest) cat
+collector chat minigame in 2 parts -- a serverside app (in `site`) and a
+clientside syncing script (meant to be ran by the streamer, in `sync_script`).
+
+Still very work-in-progress!
--- site/src/Catstat/Controllers/HomeController.php
+++ site/src/Catstat/Controllers/HomeController.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class HomeController {
-	public function index(Request $req, Response $resp, $args) {
+	public function index(Request $_req, Response $resp, array $_args): Response {
 		$resp->getBody()->write('Home controller says hello!');
 		return $resp;
 	}
