73e78cfff30adf681db975061106ec00e8848665
sleepycrow <sleepycrow@posteo.net>
2026-09-02 01:13:18 +0200

    Implement rudimentary user lookup

--- site/src/Catstat/Controllers/HomeController.php
+++ site/src/Catstat/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace Catstat\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use Catstat\Utils\UserFileUtils;
 
 class HomeController {
 	public function index(Request $req, Response $resp, array $_args): Response {
@@ -15,6 +16,27 @@ class HomeController {
 		$view = Twig::fromRequest($req);
 		$username = $req->getQueryParams()['user'] ?? null;
 		
-		return $view->render($resp, 'test.html.twig', [ 'msg' => ('Got lookup request for ' . $username . '!')]);
+		$file_path = UserFileUtils::get_path_for_user($username);
+		if (!UserFileUtils::is_valid_user_name($username) || !file_exists($file_path)) {
+			$resp->withStatus(404);
+			return $view->render($resp, 'test.html.twig', [ 'msg' => ('Not found!')]);
+		}
+
+		$file = fopen($file_path, "r");
+		if (!$file) {
+			$resp->withStatus(500);
+			return $view->render($resp, 'test.html.twig', [ 'msg' => ('Read error!')]);
+		}
+
+		$all_cats = [];
+		while (($cat = fgets($file)) !== false) {
+			array_push($all_cats, $cat);
+		}
+		fclose($file);
+		
+		return $view->render($resp, 'lookup.html.twig', [
+			'username' => $username,
+			'cats' => $all_cats,
+		]);
 	}
 }
